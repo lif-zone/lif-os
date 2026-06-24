c81b70c5ed98a16d3d8a711f69e8fdfe8a965cf7
Bright Life <bright@bright.center>
2026-06-24 23:52:58 +0300

    initial lif-os test under lif

---
+++ test/browser.js
@@ -0,0 +1,34 @@
+import assert from 'node:assert/strict';
+import {spawn} from 'node:child_process';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import puppeteer from 'puppeteer-core';
+import etask from 'lif-kernel/etask';
+import {browser_open, browser_test, server_open, fetch_test,
+} from 'lif-kernel/test/test_lib.js';
+
+const root = path.join(path.dirname(fileURLToPath(import.meta.url)), '..');
+const port = 4005;
+const url_base = `http://localhost:${port}`;
+const cmd = [root+'/server.js', '-p', ''+port];
+
+describe('browser', function(){
+  let proc, browser;
+  before(async function(){
+    this.timeout(10000);
+    proc = await server_open({cmd, search: 'Serving', cwd: root});
+    browser = await browser_open();
+  });
+  after(()=>{
+    browser?.close();
+    proc?.kill();
+  });
+  it('GET /lif-kernel/hi.js', async()=>{
+    await fetch_test({url: url_base+'/lif-kernel/hi.js', search: 'hi world'});
+  });
+  it('page /?/lif-os/', async function(){
+    this.timeout(30000);
+    await browser_test({browser, url: url_base+'/?/lif-os/',
+      search: 'LIF Wallet'});
+  });
+});
