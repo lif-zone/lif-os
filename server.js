e0e82d152bf8fe5f44f80c7c2cfed9a94aa5f134
Bright Life <bright@bright.center>
2026-08-01 12:23:55 +0300

    rm obsolete

--- server.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import server from 'lif-kernel/web/server_lib.js';
-let root = import.meta.dirname;
-let map = {};
-map['/lif-kernel'] = 'node_modules/lif-kernel';
-map['/lif-os'] = '.';
-server({map, root});
